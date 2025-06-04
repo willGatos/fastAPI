2d912490cd9c8116efcd083bede938429864b62f
William Ironmore <86080749+willGatos@users.noreply.github.com>
2025-06-04 12:26:43 -0400

    FC

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
 @app.get("/")
 async def read_root():
     return {"message": "Hola Mundo"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
